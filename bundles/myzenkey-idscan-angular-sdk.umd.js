169e986064eb003fc01d35bcf4f38932e0ea36a1
arosa <alex.rosa@myzenkey.com>
2021-11-02 23:39:50 -0400

    use regex to encode string

--- bundles/myzenkey-idscan-angular-sdk.umd.js
+++ bundles/myzenkey-idscan-angular-sdk.umd.js
@@ -311,10 +311,10 @@
                             // const licenseKey = '5809b7b7-886f-4c88-9061-59a2baf485be';
                             // const licenseKey_pilot = 'c77aa0ce-f00d-4843-86a9-38e6c1bf7d8e';
                             //return `{“first_name“:“Ben“, “last_name“:“Dover“, “last_4_social” :“3844“}`;
-                            publicKeyProvided = publicKeyProvided.replace('+', '%2B');
-                            publicKeyProvided = publicKeyProvided.replace('=', '%3D');
-                            privateKeyProvided = privateKeyProvided.replace('+', '%2B');
-                            privateKeyProvided = privateKeyProvided.replace('=', '%3D');
+                            publicKeyProvided = publicKeyProvided.replace(/\+/g, '%2B');
+                            publicKeyProvided = publicKeyProvided.replace(/\=/g, '%3D');
+                            privateKeyProvided = privateKeyProvided.replace(/\+/g, '%2B');
+                            privateKeyProvided = privateKeyProvided.replace(/\=/g, '%3D');
                             url = "https://idscan-qa.xcijv.com/auth/getDataWithSessionIdPrivateKeyPubKey/";
                             return [4 /*yield*/, axios.get(url + "?session_id=" + sessionId + "&sp_publicKey=" + publicKeyProvided + "&sp_privateKey=" + privateKeyProvided)
                                     .then(function (data) {
